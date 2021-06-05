164bacdf8a6689a276c06ebca8a506680d9f0087
yommyjooin <57985458+yommyjooin@users.noreply.github.com>
2021-06-06 03:12:18 +0900

    'info' Map에 열차 정보 추가 (시간 선택 정렬 필요)

--- app/src/main/java/com/example/termproject/ViewActivity.java
+++ app/src/main/java/com/example/termproject/ViewActivity.java
@@ -16,6 +16,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.sql.Array;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class ViewActivity extends AppCompatActivity {
 
@@ -136,7 +140,7 @@ public class ViewActivity extends AppCompatActivity {
 
                     case XmlPullParser.START_TAG:
                         tag= xpp.getName();//태그 이름 얻어오기
-
+                        /*
                         if(tag.equals("item")) ;// 첫번째 검색결과
                         else if(tag.equals("adultcharge")){
                             buffer.append("요금 : ");
@@ -153,7 +157,19 @@ public class ViewActivity extends AppCompatActivity {
                         else if(tag.equals("arrplandtime")){
                             buffer.append("도착 시간 :");
                             xpp.next();
-                            buffer.append(xpp.getText());//arrplandtime
+                            String arrplandtime = xpp.getText();
+                            String arrYear = arrplandtime.substring(0,4);
+                            buffer.append(depYear+"년");
+                            for(int i = 4; i < arrplandtime.length(); i=i+2){
+                                String temp = arrplandtime.substring(i, i+2);
+                                String str = " ";
+                                if(i == 4) str = "월";
+                                else if(i==6) str = "일";
+                                else if(i==8) str = "시";
+                                else if(i==10) str = "분";
+                                else if(i==12) str = "초";
+                                buffer.append(temp+str);
+                            }
                             buffer.append("\n");
                         }
                         else if(tag.equals("depplacename")){
@@ -162,10 +178,29 @@ public class ViewActivity extends AppCompatActivity {
                             buffer.append(xpp.getText());//depplacename
                             buffer.append("\n");
                         }
+
                         else if(tag.equals("depplandtime")){
                             buffer.append("출발 시간 :");
                             xpp.next();
                             buffer.append(xpp.getText());//depplandtime
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("depplandtime")) {
+                            buffer.append("출발 시간 :");
+                            xpp.next();
+                            String depplandtime = xpp.getText();
+                            String depYear = depplandtime.substring(0, 4);
+                            buffer.append(depYear + "년");
+                            for (int i = 4; i < depplandtime.length(); i = i + 2) {
+                                String temp = depplandtime.substring(i, i + 2);
+                                String str = " ";
+                                if (i == 4) str = "월";
+                                else if (i == 6) str = "일";
+                                else if (i == 8) str = "시";
+                                else if (i == 10) str = "분";
+                                else if (i == 12) str = "초";
+                                buffer.append(temp + str);
+                            }
                             buffer.append("\n");
                         }
                         else if(tag.equals("traingradename")){
@@ -181,7 +216,90 @@ public class ViewActivity extends AppCompatActivity {
                             buffer.append("\n");
                         }
 
+                        break;*/
+
+                        Map<String, String> info = new HashMap<String, String>();
+
+                        if(tag.equals("item"));
+                        else if(tag.equals("adultcharge")) {
+                            xpp.next();
+                            info.put("요금",xpp.getText());
+                        }
+                        else if(tag.equals("arrplacename")) {
+                            xpp.next();
+                            info.put("도착역", xpp.getText());
+                        }
+                        else if(tag.equals("arrplandtime")) {
+                            xpp.next();
+                            //info.put("도착정보", xpp.getText());
+                            String arrplandtime = xpp.getText();
+                            String arrYear = arrplandtime.substring(0,4);
+                            String arrMonth = arrplandtime.substring(4,6);
+                            String arrDay = arrplandtime.substring(6,8);
+                            String arrDate = arrYear+"/"+arrMonth+"/"+arrDay;
+
+                            String arrHour = arrplandtime.substring(8,10);
+                            String arrMin = arrplandtime.substring(10,12);
+                            String arrSec = arrplandtime.substring(12,14);
+                            String arrTime = arrHour+":"+arrMin+":"+arrSec;
+                            info.put("도착날짜", arrDate);
+                            info.put("arrHour", arrHour);
+                            info.put("도착시간", arrTime);
+
+                        }
+                        else if(tag.equals("depplacename")) {
+                            xpp.next();
+                            info.put("출발역", xpp.getText());
+                        }
+                        else if(tag.equals("depplandtime")) {
+                            xpp.next();
+                            //info.put("출발시간", xpp.getText());
+                            String depplandtime = xpp.getText();
+                            String depYear = depplandtime.substring(0,4);
+                            String depMonth = depplandtime.substring(4,6);
+                            String depDay = depplandtime.substring(6,8);
+                            String depDate = depYear+"/"+depMonth+"/"+depDay;
+
+                            String depHour = depplandtime.substring(8,10);
+                            String depMin = depplandtime.substring(10,12);
+                            String depSec = depplandtime.substring(12,14);
+                            String depTime = depHour+":"+depMin+":"+depSec;
+                            info.put("출발날짜", depDate);
+                            info.put("depHour", depHour);
+                            info.put("출발시간", depTime);
+                        }
+                        else if(tag.equals("traingradename")){
+                            xpp.next();
+                            info.put("열차", xpp.getText());
+                        }
+                        else if(tag.equals("trainno")){
+                            xpp.next();
+                            info.put("열차번호", xpp.getText());
+                        }
+
+
+                        Set<String> keys = info.keySet();
+                        for(String key:keys){
+                            buffer.append(key+":"+info.get(key));
+                            buffer.append("\n");
+                        }
+
+                        /*
+                        String temp = new String(info.get("depHour"));
+                        String temphour = new String("06");
+
+                        buffer.append("n출발역"+info.get("출발역"));
+                        buffer.append("n출발날짜"+info.get("출발날짜"));
+                        buffer.append("n출발시간"+info.get("출발시간"));
+                        buffer.append("n도착역"+info.get("도착역"));
+                        buffer.append("n도착날짜"+info.get("도착날짜"));
+                        buffer.append("n도착시간"+info.get("도착시간"));
+                        buffer.append("n열차"+info.get("열차")+" 열차번호:"+info.get("열차번호"));
+                        buffer.append("n요금"+info.get("요금"));*/
+
+
                         break;
+
 
                     case XmlPullParser.TEXT:
                         break;
